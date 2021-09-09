eed4cec7b1cd40ec2a3ac603250c07cc8d1e8519
Олексiй <alexnagorny.an@gmail.com>
2021-09-09 18:57:39 +0300

    :rocket:

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import markdownToTxt from "markdown-to-txt";
-const readme = require('../README.md')
-const App = () =>
-    <div className="App">
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <p>
-                {markdownToTxt(readme)}
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-            </a>
-        </header>
-    </div>
 
+const App = () =>
+    (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>Read README.md to start</p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 
 export default App;
